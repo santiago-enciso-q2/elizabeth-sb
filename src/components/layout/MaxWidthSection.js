43e4604f2a9e1f76703bebc71875f4c5ee62c05c
Santiago Enciso <santiago.enciso@q2.com>
2021-09-05 15:58:11 -0500

    Imported themes and removed props from @media queries.

--- src/components/layout/MaxWidthSection.js
+++ src/components/layout/MaxWidthSection.js
@@ -1,21 +1,22 @@
 import styled from "styled-components";
+import {themes} from "../styles"
 
 const MaxWidthSection = styled.section`
     grid-column: 2 / span 12;
     display: grid;
     grid-template-columns: repeat(12, minmax(auto, 4.2rem));
     grid-template-rows: max-content;
-    gap: ${(prop) => (props.gapDefault ? props.gapDefault : "2rem 2rem")};
+    gap: ${(props) => (props.gapDefault ? props.gapDefault : "2rem 2rem")};
     margin: ${(props) => (props.marginDefault ? props.marginDefault : 0)};
 
-    @media ${(props) => props.themes.breakpoints.m} {
+    @media ${themes.breakpoints.m} {
         grid-column: 2 / span 6;
         grid-template-columns: repeat(6, 1fr);
-        gap: ${(prop) => (props.gapMedium ? props.gapMedium : "1rem 1rem")};
+        gap: ${(props) => (props.gapMedium ? props.gapMedium : "1rem 1rem")};
         margin: ${(props) => (props.marginMedium ? props.marginMedium : 0)};
     }
-    @media ${(props) => props.themes.breakpoints.s} {
-        gap: ${(prop) => (props.gapSmall ? props.gapSmall : "1rem 1rem")};
+    @media ${themes.breakpoints.s} {
+        gap: ${(props) => (props.gapSmall ? props.gapSmall : "1rem 1rem")};
         margin: ${(props) => (props.marginSmall ? props.marginSmall : 0)};
     }
 `
